6b3cca1cffdc89b6b8014f003d097191be0ce78f
natapis <natapis2009@mail.ru>
2023-09-13 15:05:13 +0300

    UserCreateTest add test for double name

--- src/test/java/UserCreateTest.java
+++ src/test/java/UserCreateTest.java
@@ -6,6 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static constant.Api.BASE_URL;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 public class UserCreateTest {
     private Response loginResponse;
@@ -27,6 +28,20 @@ public class UserCreateTest {
         loginResponse = userClient.loginUser(UserCreds.credsForm(user));
         Assert.assertEquals("Не логинится", 200, loginResponse.statusCode());
     }
+    @Test
+    public void createUserDoubleNameTest() {
+        UserClient userClient = new UserClient();
+        User user = UserGenerator.requiredFields();
+        userClient.createUser(user);
+        User doubleUser = UserGenerator.doubleUserEmail(user.getEmail());
+        Response createResponse = userClient.createUser(doubleUser);
+        createResponse.then().body("message", equalTo("User already exists")).and().statusCode(403);
+ //       Assert.assertEquals("неверный статус ответа", 403, createResponse.statusCode());
+        loginResponse = userClient.loginUser(UserCreds.credsForm(doubleUser));
+        loginResponse.then().body("message", equalTo("email or password are incorrect")).and().statusCode(401);
+ //       Assert.assertEquals("Логинится, а не должен", 401, loginResponse.statusCode());
+    }
+
     @After
     public void tearDown(){
         if (loginResponse.statusCode() == 200) {
