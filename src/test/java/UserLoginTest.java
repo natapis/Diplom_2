99f35b7e8b72028c8f3210b36aab0f781950e5c6
natapis <natapis2009@mail.ru>
2023-09-13 16:24:33 +0300

    UserLoginTest fix

--- src/test/java/UserLoginTest.java
+++ src/test/java/UserLoginTest.java
@@ -8,17 +8,31 @@ import static constant.Api.BASE_URL;
 import static org.hamcrest.core.IsEqual.equalTo;
 
 public class UserLoginTest {
-    private Response loginResponse;
+//    private Response loginResponse;
     private String token;
+    private UserClient userClient = new UserClient();
+    private User user = UserGenerator.requiredFields();
     @Before
     public void setUp() {
         RestAssured.baseURI = BASE_URL;
-        loginResponse = null;
+ //       loginResponse = null;
         token = null;
+        userClient.createUser(user);
     }
 
     @Test
     public void loginExistUserTest() {
+
+//        Response createResponse = userClient.createUser(user);
+//        userClient.createUser(user);
+//        Assert.assertEquals("неверный статус ответа", 200, createResponse.statusCode());
+        Response loginResponse = userClient.loginUser(UserCreds.credsForm(user));
+        loginResponse.then().body("success", equalTo(true)).and().statusCode(200);
+//        Assert.assertEquals("Не логинится", 200, loginResponse.statusCode());
+    }
+
+    @Test
+    public void loginUserWrongLoginTest() {
         UserClient userClient = new UserClient();
         User user = UserGenerator.requiredFields();
 //        Response createResponse = userClient.createUser(user);
@@ -30,9 +44,10 @@ public class UserLoginTest {
     }
 
     public void tearDown() {
-        if (loginResponse.statusCode() == 200) {
-            token = loginResponse.body().as(LoginResponse.class).getAccessToken();
-            UserClient userClient = new UserClient();
+        Response loginResponseOld = userClient.loginUser(UserCreds.credsForm(user));
+        if (loginResponseOld.statusCode() == 200) {
+            token = loginResponseOld.body().as(LoginResponse.class).getAccessToken();
+//            UserClient userClient = new UserClient();
             Response deleteResponse = userClient.deleteUser(token.substring(7));
             System.out.println(token);
             Assert.assertEquals("Пользователь не удален", 202, deleteResponse.statusCode());
